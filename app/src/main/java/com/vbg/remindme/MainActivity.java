88d3b94ab5988d5d66f38dc6e6a8587e8376eab5
Vladislav Plemyannik <razz2zz@rambler.ru>
2016-08-26 18:42:36 +0300

    Fix viewPager

--- app/src/main/java/com/vbg/remindme/MainActivity.java
+++ app/src/main/java/com/vbg/remindme/MainActivity.java
@@ -12,6 +12,8 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.ActionBarContextView;
+import android.support.v7.widget.ToolbarWidgetWrapper;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
